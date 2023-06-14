20512c8278c537fc317d05f9a4bb73212c2b0fe6
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-06-15 01:21:06 +0300

    add script and simple parser for gendiff

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+"""
+
+This module starts gendiff tool
+
+"""
+
+from gendiff import parser
+
+
+def main():
+    print("Start gendiff")
+    parser.parse()
+
+
+if __name__ == '__main__':
+    main()
