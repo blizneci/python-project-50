63ac63fc2f07c70115625101dd47a8636e3dc848
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-06-22 18:34:20 +0300

    change gendiff script for plain json files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,12 +7,16 @@ This module starts gendiff tool
 """
 
 from gendiff import parser
+from gendiff import generate_diff
 
 
 def main():
     print("Start gendiff")
     args = parser.parse()
-    print(args)
+    first_path = args.first_file
+    second_path = args.second_file
+    diff = generate_diff(first_path, second_path)
+    print(diff)
 
 
 if __name__ == '__main__':
