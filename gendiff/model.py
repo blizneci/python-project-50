3f4ba58a9481b114b2cfb63e069905bdb772705e
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-06-29 20:49:48 +0300

    'add "make_status" function'

--- gendiff/model.py
+++ gendiff/model.py
@@ -1,14 +1,16 @@
 from typing import Iterable
 
+from gendiff.parser import in_, is_dicts, is_lists
+
 
 def make_diff_view(
         data1: dict | list[any],
         data2: dict | list[any],
         ) -> tuple:
     """Returns empty diff view"""
-    if isinstance(data1, dict) and isinstance(data2, dict):
+    if is_dicts(data1, data2):
         return dict.fromkeys(data1.keys() | data2.keys(), None)
-    elif isinstance(data1, list) and isinstance(data2, list):
+    elif is_lists(data1, data2):
         return [None] * max(len(data1), len(data2))
 
 
@@ -28,3 +30,11 @@ def make_node(status: str, value: any) -> dict:
     else:
         node['value'] = value
     return node
+
+
+def make_status(data1, data2, key):
+    if not in_(data1, key):
+        return 'added'
+    if not in_(data2, key):
+        return 'deleted'
+    return None
