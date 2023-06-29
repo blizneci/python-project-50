2255e6fc4736795f682536c4583a4f0af32463ae
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-06-29 15:19:41 +0300

    rename parser function "get_data_from_file" to "parse"

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,7 @@ This module starts gendiff tool
 
 from gendiff import cli
 from gendiff.gendiff import gen_diff
-from gendiff.parser import get_data_from_file
+from gendiff.parser import parse
 from gendiff.formatter import format_output
 
 
@@ -17,8 +17,8 @@ def main():
     first_file_path = args.first_file
     second_file_path = args.second_file
     output_format = args.FORMAT
-    first_file_data = get_data_from_file(first_file_path)
-    second_file_data = get_data_from_file(second_file_path)
+    first_file_data = parse(first_file_path)
+    second_file_data = parse(second_file_path)
     diff = gen_diff(first_file_data, second_file_data)
     format_output(diff, output_format)
 
