cd6472a41ec00bce2dbd0ad67a02fe3bb5b262bd
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-07-06 03:39:13 +0300

    refactor script and __init__.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,20 +7,16 @@ This module starts gendiff tool
 """
 
 from gendiff import cli
-from gendiff.gendiff import gen_diff
-from gendiff.parser import parse
-from gendiff.formatter import format_output
+from gendiff import generate_diff
 
 
 def main():
     args = cli.parse()
     first_file_path = args.first_file
     second_file_path = args.second_file
-    output_format = args.FORMAT
-    first_file_data = parse(first_file_path)
-    second_file_data = parse(second_file_path)
-    diff = gen_diff(first_file_data, second_file_data)
-    format_output(diff, output_format)
+    _format = args.FORMAT
+    output = generate_diff(first_file_path, second_file_path, _format)
+    print(output)
 
 
 if __name__ == '__main__':
