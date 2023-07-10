69ea62a803638a74d13bd9b98905dd26ce37aaeb
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-07-10 03:57:39 +0300

    one constructor make_ for all types of nodes

--- gendiff/model.py
+++ gendiff/model.py
@@ -13,19 +13,26 @@ UNCHANGED = 'unchanged'
 NESTED = 'nested'
 
 
-def make_(status, value):
+def make_(status, *, value=None, removed=None, added=None, children=None):
     """Makes a diff node with given status and value.
 
     Args:
-        status: A status than represents what happened with the given value.
-            Possible statuses are: 'unchanged', 'added', 'removed'.
-        value: Any type of value.
+        status: A status that represents what happened with the given value.
+            Possible statuses are:
+            'unchanged', 'added', 'removed', 'changed', 'nested'.
+        value: Value of an unchanged node.
+        removed: Value of a changed node.
+        added: Value of a changed node.
+        children: children of a nested node.
 
     Returns:
-        An object as a function, that contains status and value.
-        To get the status and value use functions below:
-        'get_status(node)' returns status of the node.
-        'get_value(node)' returns a value.
+        An object as a function, that contains status, values and children.
+        To get the status, values and children use functions below:
+        'get_status(node)' returns a status of the node.
+        'get_value(node)' returns a value of unchanged node.
+        'get_removed(node)' returns a removed value.
+        'get_added(node)' returns a added value.
+        'get_children(node)' returns children of a given nested node.
     """
     @wraps(make_)
     def inner(message):
@@ -35,70 +42,38 @@ def make_(status, value):
                 return status
             case 'get_value':
                 return value
+            case 'get_removed':
+                return removed
+            case 'get_added':
+                return added
+            case 'get_children':
+                return children
     return inner
 
 
 def make_unchanged(value):
     """Returns a diff node with status 'unchanged'."""
-    return make_(UNCHANGED, value)
+    return make_(UNCHANGED, value=value)
 
 
 def make_removed(value):
     """Returns a diff node with status 'removed'."""
-    return make_(REMOVED, value)
+    return make_(REMOVED, value=value)
 
 
 def make_added(value):
     """Returns a diff node with status 'added'."""
-    return make_(ADDED, value)
+    return make_(ADDED, value=value)
 
 
 def make_changed(removed_value, added_value):
-    """Makes a diff node with status 'chaanged' and two values.
-
-    Args:
-        removed_value: Removed value.
-        added_value: Added value.
-
-    Returns:
-        An object as a function, that contains status and changed values.
-        To get the status and values use functions below:
-        'get_status(node)' returns status of the node.
-        'get_removed(node)' returns removed value.
-        'get_added(node)' returns added value.
-    """
-    @wraps(make_changed)
-    def inner(message):
-        match message:
-            case 'get_status':
-                return CHANGED
-            case 'get_removed':
-                return removed_value
-            case 'get_added':
-                return added_value
-    return inner
+    """Return a changed diff node."""
+    return make_(CHANGED, removed=removed_value, added=added_value)
 
 
 def make_nested(children):
-    """Makes a nested diff node with status 'nested' and children as a dict.
-
-    Args:
-        children: A dictionary with nested diff nodes.
-
-    Returns:
-        An object as a function, that contains status and children.
-        To get the status and children use functions below:
-        'get_status(node)' returns status of the node.
-        'get_children(node)' returns children of the nested node.
-    """
-    @wraps(make_nested)
-    def inner(message):
-        match message:
-            case 'get_children':
-                return children
-            case 'get_status':
-                return NESTED
-    return inner
+    """Return a nested diff node."""
+    return make_(NESTED, children=children)
 
 
 def get_children(node):
@@ -128,7 +103,7 @@ def get_status(node):
 
 def is_diff(data: any):
     """Returns True if given data is a diff view, otherwise returns False."""
-    return hasattr(data, '__wrapped__')
+    return hasattr(data, '__wrapped__') and data.__name__ == 'make_'
 
 
 def to_dict(node):
@@ -138,8 +113,8 @@ def to_dict(node):
         case 'changed':
             return {
                     'status': status,
-                    'removed_value': get_removed(node),
-                    'added_value': get_removed(node),
+                    'removed': get_removed(node),
+                    'added': get_added(node),
                     }
         case 'added' | 'removed' | 'unchanged':
             return {'status': status, 'value': get_value(node)}
