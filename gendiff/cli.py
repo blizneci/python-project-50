e72ab45e0117faf296ad7f7f45320accccb93eaa
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-07-13 18:50:11 +0300

    [iss3] add chlices to format option, change help description

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -5,14 +5,19 @@ def parse():
     """Returns arguments, parsed from command line."""
     parser = argparse.ArgumentParser(
         prog='gendiff',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description='Compares two configuration files and shows a differense.',
+        usage='gendiff [options] <filepath1> <filepath2>',
     )
     parser.add_argument(
-        '-f, --format',
+        '-f', '--format',
         dest='FORMAT',
+        choices=['stylish', 'plain', 'json'],
+        metavar='[type]',
         default='stylish',
         help='set format of output',
     )
-    parser.add_argument('first_file', help='.yml | .yaml | .json file')
-    parser.add_argument('second_file', help='.yml | .yaml | .json file')
+    parser.add_argument('first_file', help=argparse.SUPPRESS)
+    parser.add_argument('second_file', help=argparse.SUPPRESS)
+    parser._optionals.title = 'Options'
     return parser.parse_args()
