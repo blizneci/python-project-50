c6dda7f677bda20d600ba2288b7febd4dd58cd2c
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-07-18 21:39:58 +0300

    remove yaml resolver setup

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -7,16 +7,6 @@ This module implements parser for data in json or yaml | yml formats.
 import json
 
 import yaml
-from yaml.resolver import Resolver
-
-resolvers = Resolver.yaml_implicit_resolvers
-
-for ch in 'Oo':
-    if len(resolvers[ch]) == 1:
-        del resolvers[ch]
-    else:
-        tag = 'tag:yaml.org, 2002:bool'
-        resolvers[ch] = [x for x in resolvers[ch] if x[0] != tag]
 
 
 def parse(data: str, data_format: str) -> dict | list:
