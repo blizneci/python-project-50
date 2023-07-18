512565e71c3887746410bcd8d5131bde5a680723
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-07-18 21:41:40 +0300

    simplify the test function

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,34 +1,55 @@
 import os
+from itertools import cycle, repeat, chain
 
 import pytest
 
 from gendiff import generate_diff
 
-PLAIN_FILES_PATH = 'tests/fixtures/plain'
-NESTED_FILES_PATH = 'tests/fixtures/nested'
-EXPECTED_FILES_SUBDIR = 'expected'
-CASE_FILES = [('file1.json', 'file2.json'), ('file1.yaml', 'file2.yml')]
-EXPECTED_FILES = ['plain_format.txt', 'json_format.txt', 'stylish_format.txt']
-FORMATS = ['plain', 'json', 'stylish']
+
+FIXTURES_PATHS = ('tests/fixtures/plain', 'tests/fixtures/nested')
+EXPECTED_FILES = ('plain_format.txt', 'stylish_format.txt', 'json_format.txt')
+FORMATS = ('plain', 'stylish', 'json')
+FIRST_FILE_NAMES = ('file1.json', 'file1.yaml')
+SECOND_FILE_NAMES = ('file2.json', 'file2.yml')
 
 
-@pytest.mark.parametrize('case_pair', CASE_FILES)
+def get_path(files):
+    for fixture_path in FIXTURES_PATHS:
+        for file_name in files:
+            path = os.path.join(fixture_path, file_name)
+            yield from repeat(path, 3)
+
+
+def get_expected():
+    for fixture_path in FIXTURES_PATHS:
+        for file_name in chain(*repeat(EXPECTED_FILES, 2)):
+            path = os.path.join(fixture_path, 'expected', file_name)
+            yield path
+
+
+def idfn(val):
+    if 'file2' in val or val in FORMATS:
+        return ''
+    path, sep, file_name = val.rpartition('/')
+    if 'expected' in path:
+        if 'plain' in path:
+            return f'plain--{file_name.removesuffix(".txt")}'
+        return f'nested--{file_name.removesuffix(".txt")}'
+    file_type = file_name.rpartition('.')[-1]
+    return file_type
+
+
 @pytest.mark.parametrize(
-    'expected_file_name, format_',
-    list(zip(EXPECTED_FILES, FORMATS)),
+    'path1, path2, format_, expected',
+    list(zip(
+        get_path(FIRST_FILE_NAMES),
+        get_path(SECOND_FILE_NAMES),
+        cycle(FORMATS),
+        get_expected(),
+    )),
+    ids=idfn,
 )
-@pytest.mark.parametrize('files_path', [PLAIN_FILES_PATH, NESTED_FILES_PATH])
-def test_generate_diff(files_path, case_pair, expected_file_name, format_):
-    path1, path2 = map(
-        lambda file_name: os.path.join(files_path, file_name),
-        case_pair,
-    )
-    expected_diff_path = os.path.join(
-        files_path,
-        EXPECTED_FILES_SUBDIR,
-        expected_file_name,
-    )
-    with open(expected_diff_path, 'r') as f:
+def test_generate_diff(path1, path2, format_, expected):
+    with open(expected, 'r') as f:
         expected_formatted_diff = f.read()
-    formatted_diff = generate_diff(path1, path2, format_)
-    assert formatted_diff == expected_formatted_diff
+    assert generate_diff(path1, path2, format_) == expected_formatted_diff
